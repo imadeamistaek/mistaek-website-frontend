ede145013d826403da784a9a8dbf393850024b10
Mistaek <imadeamistaek@gmail.com>
2024-08-30 14:47:12 +0100

    Set up Vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,10 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 import path from 'path';
 
 export default {
     kit: {
         adapter: adapter(),
-        // Other SvelteKit configurations
     },
 
     preprocess: preprocess({
