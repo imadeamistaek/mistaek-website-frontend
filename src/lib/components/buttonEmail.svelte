aaf3a2d928c1c3baed2b4f2d2a97f013640f0e66
Mistaek <imadeamistaek@gmail.com>
2024-09-02 16:06:51 +0100

    updated message from SendEmail function

--- src/lib/components/buttonEmail.svelte
+++ src/lib/components/buttonEmail.svelte
@@ -3,8 +3,26 @@
 	
 	// Define the email parameters
 	const email = 'imadeamistaek@gmail.com';
-	const subject = 'Project Inquiry';
-	const body = 'Hello, I´m interested in discussing a project with you. Thanks!';
+	const subject = "Excited to discuss a new project!";
+	const body = `
+Hi there,
+
+I’m [Your Name], and I’m reaching out because I’d love to work with you on an exciting project! Here’s a bit more about what I’m looking to achieve:
+
+Company/Organization: [Your company/organization name, if applicable]
+Project Overview: [Tell me about your project and what you’re aiming for]
+Services Needed: [Which services are you interested in?]
+Budget: [If you have a budget in mind, feel free to share it here]
+Timeline: [Do you have a deadline or timeframe in mind?]
+Referral: [How did you hear about us?]
+
+Looking forward to hearing from you!
+
+Thanks,
+[Your Name]
+[Your Position/Role]
+[Your Contact Information]
+`;
 	
 	function handleEmailClick(event) {
 		event.preventDefault(); // Prevent the default link behavior
