b71ea4eda750e1442f384a5aa69d49a959a5ff52
André Sequeira <imadeamistaek@gmail.com>
2025-01-12 17:10:46 +0000

    Added new link to the store

--- src/lib/utils/links.js
+++ src/lib/utils/links.js
@@ -2,6 +2,7 @@ export const links = [
 	{ label: 'Home', url: '/', isExternal: false },
 	{ label: 'About', url: '/about', isExternal: false },
 	{ label: 'Roadmap', url: '/roadmap', isExternal: false },
-	{ label: 'Articles', url: '/blog', isExternal: false }
+	{ label: 'Articles', url: '/blog', isExternal: false },
+	{ label: 'Store', url: 'https://mistaek.gumroad.com/', isExternal: true }
 	// Add more links as needed
 ];
